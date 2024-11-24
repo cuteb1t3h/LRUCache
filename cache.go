223819792de1056483ce5fe3280dfde3724fb44c
cuteb1t3h <cuteb1t3h@gmail.com>
2024-11-24 15:17:35 +0300

    cache and tests

---
+++ cache.go
@@ -0,0 +1,158 @@
+package lrucache
+
+import (
+	"sync"
+	"time"
+)
+
+// Node - Элемент двусвязного списка
+type Node struct {
+	key        any
+	value      any
+	exp        time.Time
+	prev, next *Node
+}
+
+type LRUCache struct {
+	items map[any]*Node
+	cap   int
+	head  *Node
+	tail  *Node
+	mutex sync.RWMutex
+}
+
+type ICache interface {
+	Cap() int
+	Len() int
+	Clear()
+	Add(key, value any)
+	AddWithTTL(key, value any, ttl time.Time)
+	Get(key any) (value any, ok bool)
+	Remove(key any)
+}
+
+// NewCache - Конструктор кеша
+func NewCache(cap int) *LRUCache {
+	cache := &LRUCache{
+		cap:   cap,
+		items: make(map[any]*Node, cap),
+	}
+	go cache.checkExpired()
+	return cache
+}
+
+func (cache *LRUCache) Cap() int {
+	c := cache.cap
+	return c
+}
+
+func (cache *LRUCache) Len() int {
+	cache.mutex.RLock()
+	l := len(cache.items)
+	cache.mutex.RUnlock()
+	return l
+}
+
+// Clear - Отчистка всего кеша
+func (cache *LRUCache) Clear() {
+	cache.mutex.Lock()
+	clear(cache.items)
+
+	for cache.head != cache.tail {
+		cache.head = cache.head.next
+		cache.head.prev = nil
+	}
+	cache.head = nil
+	cache.tail = nil
+	cache.mutex.Unlock()
+}
+
+func (cache *LRUCache) Add(key, value any) {
+	cache.add(key, value, 0)
+}
+
+// AddWithTTL - ttl должен быть больше 0
+func (cache *LRUCache) AddWithTTL(key, value any, ttl time.Duration) {
+	cache.add(key, value, ttl)
+}
+
+func (cache *LRUCache) add(key, value any, ttl time.Duration) {
+	cache.mutex.Lock()
+
+	if val, ok := cache.items[key]; !ok {
+		node := &Node{key: key, value: value}
+		if ttl != 0 {
+			node.exp = time.Now().Add(ttl)
+		}
+		if len(cache.items) == 0 {
+			cache.head = node
+			cache.tail = node
+		} else {
+			// когда кеш полный, удаляется последний элемент для добавления нового
+			if len(cache.items) == cache.cap {
+				delete(cache.items, cache.tail.key)
+				cache.tail.prev.next = nil
+				cache.tail = cache.tail.prev
+			}
+
+			node.next = cache.head
+			cache.head.prev = node
+			cache.head = node
+		}
+		cache.items[key] = node
+	} else {
+		val.value = value
+	}
+	cache.mutex.Unlock()
+}
+
+// Get - Получение элемента по ключу
+func (cache *LRUCache) Get(key any) (value any, ok bool) {
+	cache.mutex.RLock()
+	if val, ok := cache.items[key]; ok {
+		cache.mutex.RUnlock()
+		return val.value, ok
+	} else {
+		cache.mutex.RUnlock()
+		return nil, ok
+	}
+}
+
+// Remove - Удаление элемента по ключу
+func (cache *LRUCache) Remove(key any) {
+	cache.mutex.Lock()
+	if val, ok := cache.items[key]; ok {
+		if val.prev != nil {
+			val.prev.next = val.next
+		}
+		if val.next != nil {
+			val.next.prev = val.prev
+		}
+
+		delete(cache.items, key)
+	}
+	cache.mutex.Unlock()
+}
+
+// checkExpired - Проверяет истекло ли время хранения элемента кеша
+func (cache *LRUCache) checkExpired() {
+	for {
+		time.Sleep(time.Second * 5)
+		cache.mutex.Lock()
+		for key, val := range cache.items {
+			if !val.exp.IsZero() {
+				if time.Now().After(val.exp) {
+					if val.prev != nil {
+						val.prev.next = val.next
+					}
+					if val.next != nil {
+						val.next.prev = val.prev
+					}
+
+					delete(cache.items, key)
+				}
+			}
+		}
+		cache.mutex.Unlock()
+	}
+}
---
+++ cache_test.go
@@ -0,0 +1,202 @@
+package lrucache
+
+import (
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCacheInit(t *testing.T) {
+	capacity := 10
+	expected := 0
+
+	cache := NewCache(capacity)
+
+	assert.Equal(t, expected, cache.Len())
+	assert.Equal(t, capacity, cache.Cap())
+	assert.Nil(t, cache.head)
+	assert.Nil(t, cache.tail)
+}
+
+func TestGet(t *testing.T) {
+	capacity := 10
+	cache := NewCache(capacity)
+
+	cache.Add(1, "a")
+	cache.Add(2, "b")
+
+	val, ok := cache.Get(1)
+
+	assert.Equal(t, "a", val)
+	assert.True(t, ok)
+}
+
+func TestGetNotFound(t *testing.T) {
+	capacity := 10
+	cache := NewCache(capacity)
+
+	cache.Add(2, "b")
+
+	val, ok := cache.Get(1)
+
+	assert.Equal(t, nil, val)
+	assert.False(t, ok)
+}
+
+func TestAdd(t *testing.T) {
+	capacity := 10
+	cache := NewCache(capacity)
+
+	cache.Add(2, "b")
+	val, ok := cache.items[2]
+
+	assert.True(t, ok)
+	assert.Equal(t, "b", val.value)
+	assert.Equal(t, 2, val.key)
+	assert.Equal(t, val, cache.head)
+	assert.Equal(t, val, cache.tail)
+}
+
+func TestAddRewrite(t *testing.T) {
+	capacity := 10
+	cache := NewCache(capacity)
+
+	cache.Add(2, "b")
+	node := cache.items[2]
+	expected := *node
+	cache.Add(2, "a")
+	expected.value = "a"
+
+	assert.Equal(t, node, cache.items[2])
+	assert.Equal(t, expected, *cache.items[2])
+	assert.Equal(t, 2, node.key)
+}
+
+func TestAddDelete(t *testing.T) {
+	capacity := 2
+	cache := NewCache(capacity)
+
+	cache.Add(1, "a")
+	cache.Add(2, "b")
+	cache.Add(3, "c")
+
+	head := cache.head.value
+	tail := cache.tail.value
+
+	_, ok := cache.items[1]
+	assert.False(t, ok)
+
+	_, ok = cache.items[3]
+	assert.True(t, ok)
+
+	assert.Equal(t, cache.Len(), cache.Cap())
+	assert.Equal(t, "c", head)
+	assert.Equal(t, "b", tail)
+}
+
+func TestAddWithTTL(t *testing.T) {
+	capacity := 10
+	cache := NewCache(capacity)
+
+	cache.Add(2, "b")
+	val, ok := cache.items[2]
+
+	assert.True(t, ok)
+	assert.Equal(t, "b", val.value)
+	assert.Equal(t, 2, val.key)
+	assert.Equal(t, val, cache.head)
+	assert.Equal(t, val, cache.tail)
+}
+
+func TestAddWithTTLCheckDeleted(t *testing.T) {
+	capacity := 3
+	cache := NewCache(capacity)
+
+	cache.AddWithTTL(1, "a", 5*time.Second)
+	cache.AddWithTTL(2, "b", 10*time.Second)
+	cache.AddWithTTL(3, "c", 3*time.Second)
+
+	val, ok := cache.items[1]
+	assert.True(t, ok)
+	assert.Equal(t, 1, val.key)
+	assert.Equal(t, "a", val.value)
+	val, ok = cache.items[2]
+	assert.True(t, ok)
+	assert.Equal(t, 2, val.key)
+	assert.Equal(t, "b", val.value)
+	val, ok = cache.items[3]
+	assert.True(t, ok)
+	assert.Equal(t, 3, val.key)
+	assert.Equal(t, "c", val.value)
+
+	time.Sleep(5 * time.Second)
+
+	w := sync.WaitGroup{}
+	w.Add(3)
+	go func() {
+		defer w.Done()
+		time.Sleep(5 * time.Second)
+		_, ok := cache.Get(1)
+		assert.False(t, ok)
+	}()
+
+	go func() {
+		defer w.Done()
+		time.Sleep(10 * time.Second)
+		_, ok := cache.Get(2)
+		assert.False(t, ok)
+	}()
+
+	go func() {
+		defer w.Done()
+		time.Sleep(3 * time.Second)
+		_, ok := cache.Get(3)
+		assert.False(t, ok)
+	}()
+	w.Wait()
+}
+
+func TestRemove(t *testing.T) {
+	capacity := 3
+	cache := NewCache(capacity)
+
+	cache.Add(1, "a")
+	cache.Add(2, "b")
+	cache.Add(3, "c")
+
+	length := cache.Len()
+	cache.Remove(2)
+	assert.Equal(t, length-1, cache.Len())
+
+	_, ok := cache.Get(2)
+	assert.False(t, ok)
+
+	flag := true
+	head := cache.head
+	for head != cache.tail {
+		head = head.next
+		if head.key == 2 {
+			flag = false
+		}
+	}
+	assert.True(t, flag)
+
+	_, ok = cache.items[2]
+	assert.False(t, ok)
+}
+
+func TestClear(t *testing.T) {
+	capacity := 3
+	cache := NewCache(capacity)
+
+	cache.Add(1, "a")
+	cache.Add(2, "b")
+	cache.Add(3, "c")
+
+	cache.Clear()
+
+	assert.Nil(t, cache.head)
+	assert.Nil(t, cache.tail)
+	assert.Equal(t, 0, cache.Len())
+}
